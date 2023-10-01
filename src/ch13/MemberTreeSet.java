e769d0ff302da6bb2c1a870399965f434b4004f0
Dokkangs <snnam0421@naver.com>
2023-10-01 22:52:49 +0900

    자바 공부

---
+++ src/ch13/MemberTreeSet.java
@@ -0,0 +1,44 @@
+package ch13;
+
+import java.util.Iterator;
+import java.util.TreeSet;
+
+public class MemberTreeSet {
+
+	private TreeSet<Member> treeSet;
+	
+	public MemberTreeSet() {
+		treeSet = new TreeSet<Member>(new Member());
+	}
+	
+	public void addMember(Member member) {
+		treeSet.add(member);
+	}
+	
+	public boolean removeMember(int memberId) {
+		
+		//Iterator를 이용한 순회.
+		Iterator<Member> ir = treeSet.iterator();
+		
+		while(ir.hasNext()) {
+			Member member =ir.next();
+			int tempId = member.getMemberId();
+			if(tempId == memberId) {
+				treeSet.remove(member);
+				return true;
+			}
+		}
+		System.out.println(memberId + "가 존재하지 않습니다.");
+		return false;
+	}
+	
+	public void showAllMember() {
+		
+		for(Member mem:treeSet) {
+			System.out.println(mem);
+		}
+		System.out.println();
+	}
+
+	
+}
